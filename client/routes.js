1ffd0cc9d2e67d4ee4858b8bffdfd6b8b17c51bd
Yanjaa <hi@yanjaa.com>
2020-06-18 17:00:28 -0400

    feat: imported components to routes

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  AllProducts,
+  SingleProduct,
+  Cart
+} from './components'
 import {me} from './store'
 
 /**
@@ -21,6 +28,9 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/products/:id" component={SingleProduct} />
+        <Route path="/products" component={AllProducts} />
+        <Route path="/cart" component={Cart} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
@@ -29,6 +39,7 @@ class Routes extends Component {
         )}
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
+        <Route exact path="/products" component={AllProducts} />
       </Switch>
     )
   }
