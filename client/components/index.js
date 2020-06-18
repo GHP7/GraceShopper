6f48d74fbf9db7140a5f0d7d6b1a54f456bec470
Yanjaa <hi@yanjaa.com>
2020-06-18 17:48:51 -0400

    feat: order model and routes updated

--- client/components/index.js
+++ client/components/index.js
@@ -5,7 +5,7 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
+export {Login, Signup} from './auth-form'
 export {default as AllProducts} from './all-products'
 export {default as SingleProduct} from './single-product'
-export {default as AddProduct} from './addProduct'
-export {Login, Signup} from './auth-form'
+// export {default as Cart} from './cartComponent'
